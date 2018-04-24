97460e0fc5c43fd789bdc3c8d5419d4c66e4949b
103072143 <37181044+kjkj0639@users.noreply.github.com>
2018-04-24 16:23:55 +0800

    Add files via upload

--- hw5.py
+++ hw5.py
@@ -15,7 +15,7 @@ def main(filename):
             
         for word in words:
             word=word.strip(string.punctuation)
-            if word !=(" ")
+            if word!=(" "):
                 all_words.append(word)
 
     
@@ -23,16 +23,16 @@ def main(filename):
 
    
     with open('wordcount.csv','w') as csv_file:
-        writer = csv.writer(csv_file,delimite=",")
+        writer = csv.writer(csv_file,delimiter=",")
         writer.writerow(['word', 'count'])
         writer.writerows(counter.most_common())
 
     
     with open('wordcount.json','w') as json_file:
-        json.dump(counter, json_file)
+        json.dump(counter.most_common(), json_file)
 
     with open('wordcount.pkl','wb') as pkl_file:
-        pickle.dump(counter, pkl_file)
+        pickle.dump(counter.most_common(), pkl_file)
 
 if __name__ == '__main__':
     main("i_have_a_dream.txt")
